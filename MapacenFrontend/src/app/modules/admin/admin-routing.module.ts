e90b9cc13160e7678fe4d941415dbe891d7bbdcf
Filip <filipmiera1@gmail.com>
2023-01-07 00:13:40 +0100

    .

--- MapacenFrontend/src/app/modules/admin/admin-routing.module.ts
+++ MapacenFrontend/src/app/modules/admin/admin-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from '@modules/home/home.component';
-import { AdminComponent } from '../admin/admin.component';
+import { AdminComponent } from './admin.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AdminComponent
-  },
-  {
-    path: 'home',
-    component: HomeComponent
+    component: AdminComponent,
   },
 ];
 
@@ -18,4 +13,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class HomeRoutingModule { }
+export class AdminRoutingModule { }
